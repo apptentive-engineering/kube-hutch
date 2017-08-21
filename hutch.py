78f9397b881b467220763a98ab8cd3906d4c411f
Andres Rojas <andres@conpat.io>
2017-08-21 16:54:05 -0700

    Convert keys from snake_case to camelCase

--- hutch.py
+++ hutch.py
@@ -3,6 +3,7 @@
 from kubernetes import client, config
 from os import path, environ
 from pathlib import Path
+from re import sub
 from yaml import dump
 
 
@@ -25,19 +26,54 @@ def resource_to_yaml(resource, target_path):
   return file_path
 
 
-def remove_empty_from_dict(obj):
+def cleanup_resource(resource):
   """
-  Strips all null attributes from a given dictionary
+  Transforms a dictionary export of a kubernetes resource by:
+  - stripping all null attributes
+  - converting all keys from snake_case to camelCase
 
-  :param obj: dict - dictionary to parse
-  :return: dict - dictionary sripped of all null attributes
+  :param resource: dict - result of a to_dict() call on a kubernetes resource
+  :return: dict - cleaned up resource
   """
-  if type(obj) is dict:
-    return {k: remove_empty_from_dict(v) for k, v in obj.items() if v and remove_empty_from_dict(v)}
-  elif type(obj) is list:
-    return [remove_empty_from_dict(v) for v in obj if v and remove_empty_from_dict(v)]
-  else:
-    return obj
+  def remove_empty_from_dict(obj):
+    """
+    Strips all null attributes from a given dictionary
+
+    :param obj: dict - dictionary to parse
+    :return: dict - dictionary sripped of all null attributes
+    """
+    if type(obj) is dict:
+      return {k: remove_empty_from_dict(v) for k, v in obj.items() if v and remove_empty_from_dict(v)}
+    elif type(obj) is list:
+      return [remove_empty_from_dict(v) for v in obj if v and remove_empty_from_dict(v)]
+    else:
+      return obj
+
+
+  def keys_to_camel(d):
+    """
+    Convers all keys in a given dictionary from snake_case to camelCase
+
+    :param d: dict - dictionary to parse
+    :return: dict - converted dictionary
+    """
+    camelized = {}
+    for k, v in d.items():
+      export_value = v
+      if type(v) is dict:
+        export_value = keys_to_camel(v)
+      elif type(v) is list:
+        export_value = []
+        for i in v:
+          export_value.append(keys_to_camel(i))
+
+      camelized_key = sub(r'(?!^)_([a-zA-Z])', lambda x: x.group(1).upper(), k)
+      camelized[camelized_key] = export_value
+
+    return camelized
+
+
+  return keys_to_camel(remove_empty_from_dict(resource))
 
 
 
@@ -50,4 +86,4 @@ if __name__ == '__main__':
   print('Backing up DaemonSets:')
   for ds in v1beta1.list_daemon_set_for_all_namespaces().items:
     resource = v1beta1.read_namespaced_daemon_set(ds.metadata.name, ds.metadata.namespace)
-    print("\t" + resource_to_yaml(remove_empty_from_dict(resource.to_dict()), BASE_PATH))
+    print("\t" + resource_to_yaml(cleanup_resource(resource.to_dict()), BASE_PATH))
