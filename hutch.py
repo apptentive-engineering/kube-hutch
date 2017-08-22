3408f0751ba8817951ea5c6faef326fbb8dfe600
Andres Rojas <andres@conpat.io>
2017-08-21 18:38:12 -0700

    Strip blacklisted attributes from YAML exports

--- hutch.py
+++ hutch.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-from kubernetes import client
-from kubernetes import config as kube_config
+from kubernetes import client, config as kube_config
 from os import path, environ
 from pathlib import Path
 from re import sub
@@ -27,13 +26,15 @@ def resource_to_yaml(resource, target_path):
   return file_path
 
 
-def cleanup_resource(resource):
+def cleanup_resource(resource, blacklist=None):
   """
   Transforms a dictionary export of a kubernetes resource by:
   - stripping all null attributes
   - converting all keys from snake_case to camelCase
+  - stripping all attributes included in a blacklist
 
   :param resource: dict - result of a to_dict() call on a kubernetes resource
+  :param blacklist: list - nested list of dicts & strings pulled from the hutch config yaml
   :return: dict - cleaned up resource
   """
   def remove_empty_from_dict(obj):
@@ -74,7 +75,39 @@ def cleanup_resource(resource):
     return camelized
 
 
-  return keys_to_camel(remove_empty_from_dict(resource))
+  def filter(rsc, bl):
+    """
+    Strips any key/values listed in the given blacklist from the given resource
+
+    :param rsc: dict - result of a to_dict() call on a kubernetes resource
+    :param bl: list - nested list of dicts & strings pulled from the hutch config yaml
+    :return: dict - filtered resource
+    """
+    blacklist_keys = []
+    for i in bl:
+      if type(i) is dict:
+        blacklist_keys.append(next(iter(i)))
+      else:
+        blacklist_keys.append(i)
+
+    filtered = {}
+    for k, v in rsc.items():
+      if k in blacklist_keys:
+        i = blacklist_keys.index(k)
+        if type(bl[i]) is dict:
+          filtered[k] = filter(v, bl[i][k])
+      else:
+        filtered[k] = v
+
+    return filtered
+
+
+  cleaned_resource = keys_to_camel(remove_empty_from_dict(resource))
+
+  if blacklist is not None:
+    cleaned_resource = filter(cleaned_resource, blacklist)
+
+  return cleaned_resource
 
 
 
@@ -88,5 +121,10 @@ if __name__ == '__main__':
 
   print('Backing up DaemonSets:')
   for ds in v1beta1.list_daemon_set_for_all_namespaces().items:
-    resource = v1beta1.read_namespaced_daemon_set(ds.metadata.name, ds.metadata.namespace)
-    print("\t" + resource_to_yaml(cleanup_resource(resource.to_dict()), CONFIG['base_path']))
+    resource = v1beta1.read_namespaced_daemon_set(ds.metadata.name, ds.metadata.namespace).to_dict()
+    if 'blacklist' in CONFIG:
+      resource = cleanup_resource(resource, CONFIG['blacklist'])
+    else:
+      resource = cleanup_resource(resource)
+
+    print("\t" + resource_to_yaml(resource, CONFIG['base_path']))
