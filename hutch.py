99e10519de4450d6e19a70e7c95f89bf03e54883
Andres Rojas <andres@conpat.io>
2017-08-21 17:14:52 -0700

    Use a configuration YAML to set runtime options

--- hutch.py
+++ hutch.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-from kubernetes import client, config
+from kubernetes import client
+from kubernetes import config as kube_config
 from os import path, environ
 from pathlib import Path
 from re import sub
-from yaml import dump
+from yaml import dump, load
 
 
 
@@ -78,12 +79,14 @@ def cleanup_resource(resource):
 
 
 if __name__ == '__main__':
-  BASE_PATH = 'backup'
+  CONFIG_PATH = 'config.yaml'
+  with open(CONFIG_PATH, 'r') as stream:
+    CONFIG = load(stream)
 
-  config.load_kube_config(path.join(environ['HOME'], '.kube/config'))
+  kube_config.load_kube_config(path.join(environ['HOME'], '.kube/config'))
   v1beta1 = client.ExtensionsV1beta1Api()
 
   print('Backing up DaemonSets:')
   for ds in v1beta1.list_daemon_set_for_all_namespaces().items:
     resource = v1beta1.read_namespaced_daemon_set(ds.metadata.name, ds.metadata.namespace)
-    print("\t" + resource_to_yaml(cleanup_resource(resource.to_dict()), BASE_PATH))
+    print("\t" + resource_to_yaml(cleanup_resource(resource.to_dict()), CONFIG['base_path']))
